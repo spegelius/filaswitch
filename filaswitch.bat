c4818c3b74680909987e9a8dfdf2aeebd47bec42
Spegelius <spegelius@gmail.com>
2018-01-28 11:15:40 +0200

    Fix attempt for filaswitch.bat and white space

--- filaswitch.bat
+++ filaswitch.bat
@@ -1,5 +1,7 @@
 @echo off
 
+setlocal EnableExtensions EnableDelayedExpansion
+
 set ROOT_DIR=%~dp0
 
 rem Wrapper script for starting filaswitch. Needed in order to support
@@ -7,12 +9,21 @@ rem Slic3r automatic post-processing as Slic3r always puts the file path
 rem after all commans -> default argument ordering doesn't work
 
 rem switch file and hwconfig argument order
-IF "%1%"=="" (
+
+set "ARG1=%1"
+set "ARG2=%2"
+set "ARG3=%3"
+set "ARG4=%4"
+set "ARG5=%5"
+set "ARG6=%6"
+
+IF NOT DEFINED ARG1 (
     python "%ROOT_DIR%\src\filaswitch.py"
-) ELSE IF "%2%"=="" (
-    python "%ROOT_DIR%\src\filaswitch.py" "%1"
-) ELSE IF EXIST "%2%" (
-    python "%ROOT_DIR%\src\filaswitch.py" "%2" %1 %3 %4 %5 %6
+) ELSE IF NOT DEFINED ARG2 (
+    python "%ROOT_DIR%\src\filaswitch.py" %ARG1%
+) ELSE IF EXIST "%ARG2%" (
+    echo 3
+    python "%ROOT_DIR%\src\filaswitch.py" %ARG2% %ARG1% %ARG3% %ARG4% %ARG5% %ARG6%
 ) ELSE (
-    python "%ROOT_DIR%\src\filaswitch.py" "%1" %2 %3 %4 %5 %6
+    python "%ROOT_DIR%\src\filaswitch.py" %ARG1% %ARG2% %ARG3% %ARG4% %ARG5% %ARG6%
 )
